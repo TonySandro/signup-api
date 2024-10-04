69c0825de5074041e72fb1d74f856fa933a18922
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-04 17:31:40 -0300

    feat: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,7 +17,7 @@ describe("Sign up Controller", () => {
     expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
 
-  test("should return 400 if no name is provided", () => {
+  test("should return 400 if no email is provided", () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
@@ -30,5 +30,20 @@ describe("Sign up Controller", () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
+  });
+
+  test("should return 400 if no password is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
 });
