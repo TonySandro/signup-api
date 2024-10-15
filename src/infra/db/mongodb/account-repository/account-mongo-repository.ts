286761ea83228706d39e0f318fc2dc2b8c35b627
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-15 19:15:26 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.ts
@@ -0,0 +1,13 @@
+import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
+import { AccountModel } from "../../../../domain/models/account";
+import { AddAccountModel } from "../../../../domain/usecases/add-account";
+import { MongoHelper } from "../helpers/mongo-helper";
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const result = await accountCollection.insertOne(account);
+    const accountData = await accountCollection.findOne(result.insertedId);
+    return MongoHelper.map(accountData);
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { Collection, MongoClient } from "mongodb";
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
@@ -12,5 +12,17 @@ export const MongoHelper = {
   async disconnect(): Promise<void> {
     await this.client.close();
     this.client = null;
+  },
+
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.uri);
+    }
+    return this.client.db().collection(name);
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id });
   },
 };
