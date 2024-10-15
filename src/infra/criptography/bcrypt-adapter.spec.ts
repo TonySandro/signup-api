15f26c6f87d8656683a693174b30bfca677692a2
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-15 15:39:47 -0300

    test: ensure BcryptAdapter return a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import { BcryptAdapter } from "./bcrypt-adapter";
 import bcrypt from "bcrypt";
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
 interface SutTypes {
   sut: BcryptAdapter;
   salt: number;
@@ -21,5 +27,12 @@ describe("Bcrypt Adapter", () => {
 
     await sut.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+
+  test("Should return a hash on success", async () => {
+    const { sut } = makeSut();
+
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toBe("hash");
   });
 });
