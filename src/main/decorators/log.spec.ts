50a6ba1b8dca6175bb25fa5854250380b31b31ba
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-19 10:55:34 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository with correct error if controller returns a sever error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,3 +1,5 @@
+import { LogErrorRepository } from "../../data/protocols/log-error-repository";
+import { serverError } from "../../presentation/helpers/http-helper";
 import {
   Controller,
   HttpRequest,
@@ -22,17 +24,34 @@ const makeController = (): Controller => {
   return new ControllerStub();
 };
 
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async log(stack: string): Promise<void> {
+      return new Promise((resolve) => resolve());
+    }
+  }
+
+  return new LogErrorRepositoryStub();
+};
+
 interface SutType {
   sut: LogControllerDecorator;
   controllerStub: Controller;
+  logErrorRepositoryStub: LogErrorRepository;
 }
 
 const makeSut = (): SutType => {
+  const logErrorRepositoryStub = makeLogErrorRepository();
   const controllerStub = makeController();
-  const sut = new LogControllerDecorator(controllerStub);
+  const sut = new LogControllerDecorator(
+    controllerStub,
+    logErrorRepositoryStub
+  );
+
   return {
     sut,
     controllerStub,
+    logErrorRepositoryStub,
   };
 };
 
@@ -73,5 +92,29 @@ describe("Log Controller Decorator", () => {
         name: "Test",
       },
     });
+  });
+
+  test("Should call LogErrorRepository with correct error if controller returns a sever error", async () => {
+    const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
+    const logSpy = jest.spyOn(logErrorRepositoryStub, "log");
+
+    const fakeError = new Error();
+    fakeError.stack = "any_stack";
+    const error = serverError(fakeError);
+    jest
+      .spyOn(controllerStub, "handle")
+      .mockReturnValueOnce(new Promise((resolve) => resolve(error)));
+
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        email: "valid_email",
+        password: "valid_password",
+        passwordConfirmation: "valid_passwordConfirmation",
+      },
+    };
+
+    await sut.handle(httpRequest);
+    expect(logSpy).toHaveBeenCalledWith("any_stack");
   });
 });
