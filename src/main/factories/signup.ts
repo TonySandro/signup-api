ce6e68dcf608ef8f1566a15e8c07066a81a3d62c
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-19 08:57:13 -0300

    feat: ensure makeSignUpController returns the same result of the controller

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,21 +1,20 @@
-import { AddAccountRepository } from "./../../data/protocols/add-account-repository";
-import { EmailValidator } from "./../../presentation/protocols/email-validator";
 import { SignUpController } from "../../presentation/controllers/signup/signup";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account-mongo-repository";
+import { Controller } from "../../presentation/protocols";
+import { LogControllerDecorator } from "../decorators/log";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const addAccountRepository = new AccountMongoRepository();
   const bcryptAdapter = new BcryptAdapter(12);
   const addAccount = new DbAddAccount(bcryptAdapter, addAccountRepository);
   const emailValidatorAdapter = new EmailValidatorAdapter();
-
   const signUpController = new SignUpController(
     emailValidatorAdapter,
     addAccount
   );
 
-  return signUpController;
+  return new LogControllerDecorator(signUpController);
 };
