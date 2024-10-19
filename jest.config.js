feeceae9e6a525fbe1d8f364072ace0c21fbd249
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-18 21:08:10 -0300

    chore: removing unnecessary files from test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   clearMocks: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/**/*protocols.ts",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/presentation/protocols/index.ts"
+  ],
   coverageDirectory: "coverage",
-  coverageProvider: "v8",
   testEnvironment: "node",
   preset: "@shelf/jest-mongodb",
   transform: {
