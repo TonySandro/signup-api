70cec468cafb56a927c8ee9858fb4b07a0af0c0c
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-30 16:07:11 -0300

    refactor: change port to 5353

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,7 +4,7 @@ import env from "./config/env";
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import("./config/app")).default;
-    app.listen(5050, () =>
+    app.listen(env.port, () =>
       console.log(`server running at http://localhost:${env.port}`)
     );
   })
