f0d4d7f15e7f972cd14a71f125ff1c9a44fd9915
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-11-01 17:32:28 -0300

    test: RequiredFieldValidation not return if validation success

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -16,4 +16,11 @@ describe("Required Fields Validation", () => {
     const error = sut.validate({ name: "any_name" });
     expect(error).toEqual(new MissingParamError("field"));
   });
+
+  test("Should not return if validation success", () => {
+    const { sut } = makeSut();
+
+    const error = sut.validate({ field: "any_name" });
+    expect(error).toBeFalsy();
+  });
 });
